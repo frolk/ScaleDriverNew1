bc2f853f88c4865be28761b77c0ec7160e05c07c
frolk <frolkin-stas@yandex.ru>
2016-03-05 10:42:25 +0500

    not Send debug

--- main.c
+++ main.c
@@ -9,11 +9,46 @@
 float ScaleValue;
 float ScaleValueChange;
 int ScaleValueDetect;
-uint8_t StrScaleValueDetect[5];
-uint8_t StrPWMValue[5];
-uint8_t StrOCR[5];
+uint8_t StrScaleValueDetect[6];
+uint8_t StrPWMValue[6];
+uint8_t StrOCR[6];
 uint8_t DebugAskAnswer[16];
 
+char* shift_and_mul_utoa16(uint16_t n, char *buffer)
+{
+	uint8_t d4, d3, d2, d1, q, d0;
+
+	d1 = (n>>4)  & 0xF;
+	d2 = (n>>8)  & 0xF;
+	d3 = (n>>12) & 0xF;
+
+	d0 = 6*(d3 + d2 + d1) + (n & 0xF);
+	q = (d0 * 0xCD) >> 11;
+	d0 = d0 - 10*q;
+
+	d1 = q + 9*d3 + 5*d2 + d1;
+	q = (d1 * 0xCD) >> 11;
+	d1 = d1 - 10*q;
+
+	d2 = q + 2*d2;
+	q = (d2 * 0x1A) >> 8;
+	d2 = d2 - 10*q;
+
+	d3 = q + 4*d3;
+	d4 = (d3 * 0x1A) >> 8;
+	d3 = d3 - 10*d4;
+
+	char *ptr = buffer;
+	*ptr++ = ( d4 + '0' );
+	*ptr++ = ( d3 + '0' );
+	*ptr++ = ( d2 + '0' );
+	*ptr++ = ( d1 + '0' );
+	*ptr++ = ( d0 + '0' );
+	*ptr = 0;
+
+	while(buffer[0] == '0') ++buffer;
+	return buffer;
+}
 
 int main(void)
 {
@@ -55,12 +90,13 @@ int main(void)
 		
 		if(DebugAsk)
 			{
-				StrScaleValueDetect[5] = itoa(ScaleValueDetect,StrScaleValueDetect,10);
-				StrPWMValue[5] = itoa(PWMvalue, StrPWMValue, 10);
-				StrOCR[5] = itoa(OCR2A, StrOCR, 10);
+				shift_and_mul_utoa16(ScaleValueDetect, StrScaleValueDetect);
+				StrPWMValue[5] = itoa (PWMvalue, StrPWMValue, 10);
+				//shift_and_mul_utoa16(StrOCR, OCR2A);
 				BL_SendStr(StrScaleValueDetect);
 				BL_SendStr(StrPWMValue);
-				BL_SendStr(StrOCR);
+				//BL_SendStr(StrOCR);
+
 				DebugAsk = 0;
 			}
 				
